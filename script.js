e2b352d945f24cf557ef1a1a72748b38ebe7afce
urud.theo@gmail.com <urud.theo@gmail.com>
2024-02-14 13:30:44 +0100

    js en cours

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const lvlContainer = document.getElementById("lvl");
 const pointsAvailableContainer = document.getElementById("pointsAvailable");
 
+let addBtn = document.querySelectorAll(".add");
+let reduceBtn = document.querySelectorAll(".reduce");
+let counterSpan = document.querySelectorAll(".valueAttribute");
+
+let lvl;
+let pointsAvailable;
+
 const points = () => {
   lvl = 1;
-  pointsAvailable = 5;
-  for (i = 0; i < 10; i++) {
-    lvl++;
-    pointsAvailable++;
-    if (lvl++) {
-      pointsAvailable++;
-    }
+  pointsAvailable = lvl * 5;
+
+  if (lvl++) {
+    pointsAvailable += 5;
   }
+
   lvlContainer.textContent = lvl;
   pointsAvailableContainer.textContent = pointsAvailable;
 };
 points();
+
+for (i = 0; i < addBtn.length; i++) {
+  counterSpan.value = 12;
+  addBtn[i].addEventListener("click", () => {
+    counterSpan.value++;
+    counterSpan.textContent = `${counterSpan.value}`;
+    console.log(counterSpan.value);
+  });
+}
